d6c93dd4c47d7c5dd4701d84594d17718f8f9b2c
zlatoverov <zlatoverov@haulmont.com>
2023-02-26 13:19:29 +0400

    Init Spring Data MongoDB project

---
+++ data-mongodb/src/test/java/com/sbu/dm/DataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbu.dm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
